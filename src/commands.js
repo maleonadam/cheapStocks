c04817369855bc667ac4b330bd1e758504d3ea9e
maldeve <malandro624@gmail.com>
2020-08-24 20:32:50 +0300

    handle errors

--- src/commands.js
+++ src/commands.js
@@ -1,19 +1,25 @@
 const chalk = require("chalk");
 const data = require("../currency.json");
 
+// function to fetch data and loop through our data
 const getCountry = (isocode) => {
-    data.map((datamap) => {
-        if (datamap.iso_code == isocode) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].iso_code == isocode) {
             const details = {
-                country: datamap.country,
-                currency: datamap.currency,
-                iso_4217_code: datamap.iso_code,
+                country: data[i].country,
+                currency: data[i].currency,
+                iso_4217_code: data[i].iso_code,
             };
             console.log(details);
+            break;
+        } else {
+            console.log(chalk.red("ISO Code is not supported!"));
+            break;
         }
-    });
+    }
 };
 
+// export the function so as to import in the index.js file
 module.exports = {
     getCountry,
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const program = require("commander");
 const { getCountry } = require("./commands");
 
+// use program library to set version and description for the app
 program.version("1.0.0").description("Command line Currency Application");
 
+// specify how to run commands in the CLI
 program
     .command("details <isocode>")
     .alias("d")
     .description("view currency details of a specified country")
     .action((isocode) => getCountry(isocode));
 
+// read all parameters and arguments to the program
 program.parse(process.argv);
